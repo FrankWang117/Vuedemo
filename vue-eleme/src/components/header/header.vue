fb78e04f54dcfffa37c2cde2c0a95f291f23798b
Tomorrowan <wsgoodman@163.com>
2017-12-10 15:15:55 +0800

    项目构建
    仿照饿了么app,解决了本地数据获取问题.

---
+++ vue-eleme/src/components/header/header.vue
@@ -0,0 +1,13 @@
+<template>
+	<div class="header">
+		This is header
+	</div>
+</template>
+
+<script type="text/ecmascript-6">
+export default {};
+</script>
+
+<style rel="stylesheet" lang="scss">
+
+</style>
