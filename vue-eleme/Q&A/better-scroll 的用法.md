4b3ac3bcc581937b0e0314edb8055c94b3cd3877
Tomorrowan <wsgoodman@163.com>
2017-12-14 23:22:58 +0800

    🤞better-scroll 的应用
    1.✔使用better-scroll 完成左右两栏滚动联动问题;2.✔vue中ref的应用;

---
+++ vue-eleme/Q&A/better-scroll 的用法.md
@@ -0,0 +1,3 @@
+# `better-scroll` 的用法
+
+better-scroll 涉及到 vue中 ref的使用,以及驼峰法和大写字母法命名规则的切换.
--- vue-eleme/src/components/goods/goods.vue
+++ vue-eleme/src/components/goods/goods.vue
@@ -1,8 +1,8 @@
 <template>
 	<div class = "goods">
-		<div class="menu-wrapper">
+		<div class="menu-wrapper" ref="mwrapper">
 			<ul>
-				<li v-for="item in goods" class="menu-item">
+				<li v-for="(item,index) in goods" class="menu-item" :class = "index === currentIndex? 'current':''" @click = "selectMenu(index,$event)">
 					<span class="text border-1px">
 						<span v-show="item.type > 0" class="icon" :class="classMap[item.type]"></span>
 					{{item.name}}
@@ -10,9 +10,9 @@
 				</li>
 			</ul>
 		</div>
-		<div class="foods-wrapper">
+		<div class="foods-wrapper" ref="fwrapper">
 			<ul>
-				<li class="foot-list" v-for="(item,index,key) in goods" :key="key">
+				<li class="foot-list food-list-hook" v-for="(item,index,key) in goods" :key="key">
 					<h1 class="title">{{item.name}}</h1>
 					<ul>
 						<li class="food-item border-1px" v-for="(food,index,key) in item.foods" :key="key">
@@ -23,12 +23,12 @@
 								<h2 class="name">{{food.name}}</h2>
 								<p class="desc">{{food.description}}</p>
 								<div class="extra">
-									<span class="count">月售{{food.sellCount}}</span>
+									<span class="count">月售{{food.sellCount}}份</span>
 									<span>好评率{{food.rating}}%</span>
 								</div>
 								<div class="price">
 									<span class="now">$ {{food.price}}</span>
-									<span class="old" v-show="food.oldPrice"></span>
+									<span class="old" v-show="food.oldPrice">${{food.oldPrice}}</span>
 								</div>
 							</div>
 						</li>
@@ -39,7 +39,8 @@
 	</div>
 </template>
 
-<script type="text/ecmascript-6">
+<script>
+import BScroll from 'better-scroll';
 const ERR_OK = 0;
 	export default {
 		props: {
@@ -49,18 +50,70 @@ const ERR_OK = 0;
 		},
 		data () {
 			return {
-				goods: []
+				goods: [],
+				listHeight: [],
+				scrollY: 0
 			};
 		},
+		computed: {
+			currentIndex () {
+				for (let i = 0; i < this.listHeight.length; i++) {
+					let height1 = this.listHeight[i];
+					let height2 = this.listHeight[i + 1];
+					if (!height2 || (this.scrollY >= height1 && this.scrollY < height2)) {
+						return i;
+					}
+				}
+				return 0;
+			}
+		},
+		methods: {
+			_initScroll () {
+			this.menuScroll = new BScroll(this.$refs.mwrapper, {
+				click: true
+			});
+			this.foodsScroll = new BScroll(this.$refs.fwrapper, {probeType: 3
+			});
+			this.foodsScroll.on('scroll', (pos) => {
+				this.scrollY = Math.abs(Math.round(pos.y));
+			});
+		},
+			_calculateHeight () {
+				let foodList = this.$refs.fwrapper.getElementsByClassName('food-list-hook');
+				let foodgao;
+				foodgao = 0;
+				this.listHeight.push(foodgao);
+				for (let i = 0; i < foodList.length; i++) {
+					let item = foodList[i];
+					foodgao += item.clientHeight;
+					this.listHeight.push(foodgao);
+				}
+			},
+			selectMenu (index) {
+				if (!event._constructed) {
+					return;
+				}
+				let foodList = this.$refs.fwrapper.getElementsByClassName('food-list-hook');
+				let el = foodList[index];
+				this.foodsScroll.scrollToElement(el, 300);
+			}
+	},
 		created () {
 			this.classMap = ['decrease', 'discount', 'special', 'invoice', 'guarantee'];
 			this.$http.get('/api/goods').then((response) => {
 				response = response.data;
 				if (response.errno === ERR_OK) {
 					this.goods = response.data;
+					this.$nextTick(() => {
+						this._initScroll();
+						this._calculateHeight();
+					});
 				}
 			});
-		}
+		},
+		mounted () {
+
+	}
 	};
 </script>
 
@@ -84,6 +137,16 @@ const ERR_OK = 0;
 			width: 56px;
 			line-height: 14px;
 			padding: 0 12px;
+			&.current {
+				position: relative;
+				z-index: 10;
+				margin-top: -1px;
+				background: #fff;
+				font-weight: 700;
+				.text {
+					@include border-none();
+				}
+			}
 			.icon {
 				display: inline-block;
 				vertical-align: top;
@@ -159,7 +222,7 @@ const ERR_OK = 0;
 					margin-bottom: 8px;
 				}
 				.extra {
-					&.count {
+					.count {
 						margin-right: 12px;
 					}
 				}
