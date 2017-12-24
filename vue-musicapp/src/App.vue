5dc3e40bab43b0649edabce28e2845b0809d902d
Tomorrowan <wsgoodman@163.com>
2017-12-24 20:43:54 +0800

    将准备工作完成
    完成准备工作

---
+++ vue-musicapp/src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <div id="app">
+    <h1>Hello,World</h1>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="scss">
+@import './common/scss/variable.scss';
+#app {
+  color: $color-theme;
+}
+</style>
